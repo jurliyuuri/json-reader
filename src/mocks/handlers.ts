64b4c7d6e0357bd9bacc7d545027e60b681470c6
meloviliju <ritterderschwerter178@gmail.com>
2024-03-28 19:52:01 +0900

    テスト環境にはmocksも必要だった

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,62 @@
+import { http, HttpResponse } from 'msw'
+
+export const handlers = [
+  http.get('http://jurliyuuri.com/ail/ail_compressed.json', () => {
+    return HttpResponse.json({
+      words: [{
+        entry: {
+          id: 1,
+          form: 'data'
+        },
+        translations: [{
+          title: '訳',
+          forms: ['訳1', '訳2']
+        }],
+        tags: ['テストケース'],
+        contents: [{
+          title: '内容',
+          text: '内容がここに記述される'
+        }],
+        variations: [{
+          title: '複数形',
+          forms: 'datas'
+        }],
+        relations: [{
+          title: '単数形',
+          entry: {
+            id: 2,
+            forms: 'datum'
+          }
+        }]
+      }, {
+        entry: {
+          id: 2,
+          form: 'datum'
+        },
+        translations: [{
+          title: '訳',
+          forms: ['訳1', '訳2']
+        }],
+        tags: ['テストケース'],
+        contents: [{
+          title: '内容',
+          text: '内容がここに記述される'
+        }],
+        variations: [{
+          title: '複数形',
+          forms: 'data'
+        }, {
+          title: '複数形',
+          forms: 'datas'
+        }],
+        relations: [{
+          title: '複数形',
+          entry: {
+            id: 1,
+            forms: 'data'
+          }
+        }]
+      }]
+    })
+  })
+]
