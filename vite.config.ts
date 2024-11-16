64fd2f7184c46bd44ce52951dde2a8531b0481a5
meloviliju <ritterderschwerter178@gmail.com>
2024-11-16 11:56:59 +0900

    chore!: update package
    designate pnpm@9.13.0
    
    BREAKING CHANGE: use flatConfig for eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-import { configDefaults } from 'vitest/config'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { configDefaults } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
